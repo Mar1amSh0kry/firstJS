d8a7890ca85883e0f76505ccfc8627525a999373
Mar1amSh0kry <mariomah181@gmail.com>
2024-12-01 10:04:54 +0200

    challenge v17

---
+++ main.js
@@ -0,0 +1,102 @@
+   //document.querySelector("h1").style.color='Green';
+
+
+   /* window.onload = function() {
+   document.querySelector("h1").style.color = "Green";
+   }*/
+    // 1- Alert
+    // window.alert("JS file");
+
+    // we won't use alert, we use pop up
+
+    // 2- Document  
+   /*
+    // document.write("<h1>hi</h1>")
+    // we won't use document, we use create element
+    // document.createElement("")
+    */
+   //video 888888888888888888888888
+   //    console.log("hi");
+    // console.error("errorrrrrrr");
+    // console.table(["m","n","o","p"," q"]);
+    // console.log("%cstyled %cmessage" , "color:red; font-size:40px" , "color:blue ; font-size:40px")
+
+    //video 99999999999999999999999999999
+    // ES6 (ECMAScript)
+
+   // console.log("video 100000000000000000") ;
+   // /*data types intro
+   // 1- string
+   // 2- number
+   // 3- array => object
+   // 4-object
+   // 5-boolean
+   //  */
+   // console.log("sdh dddddddddd");
+   // console.log(typeof("fgr dfnbes"));
+   // console.log(typeof 5000);
+   // console.log(typeof 5000.99);
+   // console.log(typeof [12,13,11,3]);
+   // console.log(typeof ["as" , "aq" , "er" , "df"]);
+   // console.log(typeof{name:'ma' , age:18 , country:'Eg'});
+   // console.log(typeof true);
+   // console.log(typeof false);
+   // console.log(typeof undefined);
+   // console.log(typeof null);
+
+   // console.log("video 111111111111111111111");
+
+   // /*
+   // - Declare A Variable And Use
+   // - Syntax ( Keyword | Variable Name | Assignment Operator | Variable Value )
+   // - Variable Without Var
+   // - Multiple Variables In The Same Line
+   // - Id is a Global Variable we can use it
+   // - Loosely Typed vs Strongly Typed
+   //  */ 
+
+   // var user = "mariam" , age =19 ;
+
+   // console.log(user,age);
+   // console.log(user);
+   // console.log(user);
+   // console.log(user,age);
+   // console.log(user);
+
+   // console.log("video 1222222222222222");
+
+   // console.log("in JS we use camel case");
+
+   // console.log("video 13333333333333333333333333");
+   // console.log("var, let & const");
+
+   // console.log("video 144444444444444444444");
+   // console.log('kjn');
+   // console.log("k\j\nn\"");
+
+   // console.log("video 155555555555555555555");
+   // console.log("concatenation");
+
+   // console.log("video 1666666666666666666666666666666666");
+   // let a = "eff", b = "dfb", c = "lko", d = "wbf";
+
+   
+   // console.log(`${a} "" '' \\ ${b} ${c} ${d}`);
+
+   // console.log("video 177777777777777777777777777777");
+
+let pageTitle = "Elzero", pageDescription = "Elzero Web School", pageDate = "25/10";
+   
+let card = `
+<div class = "card">
+   <div class="child">
+      <h3>  ${pageTitle}<\h3>
+      <p class = "desc">${pageDescription}<\p>
+      <span>${pageDate}<\span>
+   <\div>
+<\div>
+`
+document.write(card);
+document.write(card);
+document.write(card);
+document.write(card);
